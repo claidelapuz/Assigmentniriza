f132fcee3f97939486edeeb48676d54d86313780
claidelapuz <claidelapuz.202300335@gmail.com>
2024-10-24 23:39:51 +0800

    update

---
+++ script.js
@@ -0,0 +1,17 @@
+(function(){
+    emailjs.init("YOUR_API_PUBLIC_key");
+})();
+
+
+// Function to send email using EmailJS
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Send the email using the form data
+    console.log(this)
+    emailjs.sendForm(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, this)
+        .then(function() {
+            alert('Message Sent Successfully!');
+        }, function(error) {
+            alert('Failed to send the message: ' + JSON.stringify(error));
+        });
+});
